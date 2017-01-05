1dc900f88ab8ff6a3cea0c35dc93c20a380b68cc
Mark Lee <electronjs@lazymalevolence.com>
2017-01-04 20:18:47 -0800

    Export packager hook

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import * as configParser from './config-parser';
 import CompilerHost from './compiler-host';
 import FileChangedCache from './file-change-cache';
 import CompileCache from './compile-cache';
+import packagerCompileHook from './packager-hook';
 
 module.exports = Object.assign({},
   configParser,
-  { CompilerHost, FileChangedCache, CompileCache }
+  { CompilerHost, FileChangedCache, CompileCache, packagerCompileHook }
 );
